551dc82719fd7adc11ca9e80fad5cc77c186a1b9
Ziv Attias <ziv.attias7@gmail.com>
2023-04-22 20:11:28 +0300

    modify: MUI components, apply: Vest form validations, add: visibility icons

--- client/src/components/AuthModal/RegisterModal.tsx
+++ client/src/components/AuthModal/RegisterModal.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { LoadingButton } from "@mui/lab";
 import {
-    Button,
     Divider,
-    FormControl,
-    Input,
-    InputLabel,
     Modal,
-    ModalProps,
     Box,
     Typography,
+    TextField,
+    InputAdornment,
+    CircularProgress,
+    Button,
 } from "@mui/material";
 import { FormValues } from "./AuthModal";
+import { registerSuite } from "../../utils/suites/registerSuite";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 interface RegisterModalProps {
     open: boolean;
@@ -38,9 +40,51 @@ function RegisterModal({
     registerFormValues,
     setRegisterFormValues,
 }: RegisterModalProps) {
+    const [formState, setFormState] = React.useState({});
+    const [userPending, setUserPending] = React.useState<boolean>(false);
+    const [emailPending, setEmailPending] = React.useState<boolean>(false);
+    const [visibility, setVisibility] = React.useState<boolean>(false);
+
+    const handleChange = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        currentField: string,
+        value: string
+    ) => {
+        setRegisterFormValues({
+            ...registerFormValues,
+            [currentField]: event.target.value,
+        });
+        const nextState = { ...formState, [currentField]: value };
+        const result = registerSuite(nextState, currentField);
+        setFormState(nextState);
+
+        if (currentField === "username") {
+            setUserPending(true);
+        }
+
+        if (currentField === "email") {
+            setEmailPending(true);
+        }
+
+        result.done(() => {
+            setUserPending(false);
+            setEmailPending(false);
+        });
+    };
+
+    const suiteResult = registerSuite.get();
+
+    console.log(suiteResult.getErrors());
+
     return (
         <React.Fragment>
-            <Modal open={open} onClose={onClose}>
+            <Modal
+                open={open}
+                onClose={() => {
+                    onClose();
+                    registerSuite.reset();
+                }}
+            >
                 <Box sx={style}>
                     <Box>
                         <Typography
@@ -77,99 +121,217 @@ function RegisterModal({
                             Welcome to Homey! You're just a step away from
                             joining us.
                         </Typography>
-                        <FormControl sx={{ marginBottom: "1em" }}>
-                            <InputLabel htmlFor="first_name">
-                                First Name
-                            </InputLabel>
-                            <Input
-                                type="text"
-                                value={registerFormValues.first_name}
-                                onChange={(event) =>
-                                    setRegisterFormValues({
-                                        ...registerFormValues,
-                                        first_name: event.target.value,
-                                    })
-                                }
-                                id="first_name"
-                            />
-                        </FormControl>
-                        <FormControl sx={{ marginBottom: "1em" }}>
-                            <InputLabel htmlFor="last_name">
-                                Last Name
-                            </InputLabel>
-                            <Input
-                                type="text"
-                                value={registerFormValues.last_name}
-                                onChange={(event) =>
-                                    setRegisterFormValues({
-                                        ...registerFormValues,
-                                        last_name: event.target.value,
-                                    })
-                                }
-                                id="last_name"
-                            />
-                        </FormControl>
-                        <FormControl sx={{ marginBottom: "1em" }}>
-                            <InputLabel htmlFor="email">
-                                Email Address
-                            </InputLabel>
-                            <Input
-                                type="email"
-                                value={registerFormValues.email}
-                                onChange={(event) =>
-                                    setRegisterFormValues({
-                                        ...registerFormValues,
-                                        email: event.target.value,
-                                    })
-                                }
-                                id="email"
-                            />
-                        </FormControl>
-                        <FormControl sx={{ marginBottom: "1em" }}>
-                            <InputLabel htmlFor="username">Username</InputLabel>
-                            <Input
-                                type="text"
-                                value={registerFormValues.username}
-                                onChange={(event) =>
-                                    setRegisterFormValues({
-                                        ...registerFormValues,
-                                        username: event.target.value,
-                                    })
-                                }
-                                id="username"
-                            />
-                        </FormControl>
-                        <FormControl sx={{ marginBottom: "1em" }}>
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input
-                                type="password"
-                                value={registerFormValues.password}
-                                onChange={(event) =>
-                                    setRegisterFormValues({
-                                        ...registerFormValues,
-                                        password: event.target.value,
-                                    })
-                                }
-                                id="password"
-                            />
-                        </FormControl>
-                        <FormControl sx={{ marginBottom: "1em" }}>
-                            <InputLabel htmlFor="confirm_password">
-                                Confirm Password
-                            </InputLabel>
-                            <Input
-                                type="password"
-                                value={registerFormValues.confirm_password}
-                                onChange={(event) =>
-                                    setRegisterFormValues({
-                                        ...registerFormValues,
-                                        confirm_password: event.target.value,
-                                    })
-                                }
-                                id="confirm_password"
-                            />
-                        </FormControl>
-                        <LoadingButton loading={loading} type="submit">
+                        <TextField
+                            color={
+                                suiteResult.isValid("first_name")
+                                    ? "success"
+                                    : "error"
+                            }
+                            placeholder="First Name"
+                            error={
+                                suiteResult.hasErrors("first_name")
+                                    ? true
+                                    : false
+                            }
+                            helperText={suiteResult.getErrors("first_name")[0]}
+                            type="text"
+                            value={registerFormValues.first_name}
+                            onChange={(event) =>
+                                handleChange(
+                                    event,
+                                    "first_name",
+                                    event.target.value
+                                )
+                            }
+                            id="first_name"
+                            sx={{ marginBottom: "1em" }}
+                        />
+                        <TextField
+                            color={
+                                suiteResult.isValid("last_name")
+                                    ? "success"
+                                    : "error"
+                            }
+                            placeholder="Last Name"
+                            error={
+                                suiteResult.hasErrors("last_name")
+                                    ? true
+                                    : false
+                            }
+                            helperText={suiteResult.getErrors("last_name")[0]}
+                            type="text"
+                            value={registerFormValues.last_name}
+                            onChange={(event) =>
+                                handleChange(
+                                    event,
+                                    "last_name",
+                                    event.target.value
+                                )
+                            }
+                            id="last_name"
+                            sx={{ marginBottom: "1em" }}
+                        />
+                        <TextField
+                            InputProps={
+                                emailPending
+                                    ? {
+                                          endAdornment: (
+                                              <InputAdornment position="end">
+                                                  <CircularProgress size={20} />
+                                              </InputAdornment>
+                                          ),
+                                      }
+                                    : {}
+                            }
+                            color={
+                                suiteResult.isValid("email")
+                                    ? "success"
+                                    : "error"
+                            }
+                            placeholder="Email Address"
+                            error={
+                                suiteResult.hasErrors("email") ? true : false
+                            }
+                            helperText={suiteResult.getErrors("email")[0]}
+                            type="text"
+                            value={registerFormValues.email}
+                            onChange={(event) => {
+                                setRegisterFormValues({
+                                    ...registerFormValues,
+                                    email: event.target.value,
+                                });
+                            }}
+                            onBlur={(event) => {
+                                event.target.value && setEmailPending(true);
+                                handleChange(
+                                    event,
+                                    "email",
+                                    event.target.value
+                                );
+                            }}
+                            id="email"
+                            sx={{ marginBottom: "1em" }}
+                        />
+                        <TextField
+                            InputProps={
+                                userPending
+                                    ? {
+                                          endAdornment: (
+                                              <InputAdornment position="end">
+                                                  <CircularProgress size={20} />
+                                              </InputAdornment>
+                                          ),
+                                      }
+                                    : {}
+                            }
+                            color={
+                                suiteResult.isValid("username")
+                                    ? "success"
+                                    : "error"
+                            }
+                            placeholder="Username"
+                            error={
+                                suiteResult.hasErrors("username") ? true : false
+                            }
+                            helperText={suiteResult.getErrors("username")[0]}
+                            type="text"
+                            value={registerFormValues.username}
+                            onChange={(event) => {
+                                setRegisterFormValues({
+                                    ...registerFormValues,
+                                    username: event.target.value,
+                                });
+                            }}
+                            onBlur={(event) => {
+                                event.target.value && setUserPending(true);
+                                handleChange(
+                                    event,
+                                    "username",
+                                    event.target.value
+                                );
+                            }}
+                            id="username"
+                            sx={{ marginBottom: "1em" }}
+                        />
+                        <TextField
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <Box
+                                            component="button"
+                                            type="button"
+                                            sx={{
+                                                backgroundColor: "transparent",
+                                                border: "none",
+                                            }}
+                                            onClick={() =>
+                                                setVisibility(!visibility)
+                                            }
+                                        >
+                                            {visibility ? (
+                                                <VisibilityOffIcon />
+                                            ) : (
+                                                <VisibilityIcon />
+                                            )}
+                                        </Box>
+                                    </InputAdornment>
+                                ),
+                            }}
+                            color={
+                                suiteResult.isValid("password")
+                                    ? "success"
+                                    : "error"
+                            }
+                            placeholder="Password"
+                            error={
+                                suiteResult.hasErrors("password") ? true : false
+                            }
+                            helperText={suiteResult.getErrors("password")[0]}
+                            type={visibility ? "text" : "password"}
+                            value={registerFormValues.password}
+                            onChange={(event) =>
+                                handleChange(
+                                    event,
+                                    "password",
+                                    event.target.value
+                                )
+                            }
+                            id="password"
+                            sx={{ marginBottom: "1em" }}
+                        />
+                        <TextField
+                            color={
+                                registerFormValues.confirm_password &&
+                                suiteResult.isValid("confirm_password")
+                                    ? "success"
+                                    : "error"
+                            }
+                            placeholder="Confirm Password"
+                            error={
+                                suiteResult.hasErrors("confirm_password")
+                                    ? true
+                                    : false
+                            }
+                            helperText={
+                                suiteResult.getErrors("confirm_password")[0]
+                            }
+                            type={visibility ? "text" : "password"}
+                            value={registerFormValues.confirm_password}
+                            onChange={(event) =>
+                                handleChange(
+                                    event,
+                                    "confirm_password",
+                                    event.target.value
+                                )
+                            }
+                            id="confirm_password"
+                            sx={{ marginBottom: "1em" }}
+                        />
+                        <LoadingButton
+                            disabled={!suiteResult.isValid()}
+                            loading={loading}
+                            type="submit"
+                        >
                             Register
                         </LoadingButton>
                     </Box>
