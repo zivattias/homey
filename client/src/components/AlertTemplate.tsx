67eb5592744b7decb5a941455cca7dc41535c73b
Ziv Attias <ziv.attias7@gmail.com>
2023-04-30 00:38:06 +0300

    feature: cross-app Alerts

---
+++ client/src/components/AlertTemplate.tsx
@@ -0,0 +1,80 @@
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ErrorIcon from "@mui/icons-material/Error";
+import CloseIcon from "@mui/icons-material/Close";
+import { AlertTemplateProps } from "react-alert";
+import { Box } from "@mui/material";
+
+const AlertTemplate = ({
+  message,
+  options,
+  style,
+  close,
+}: AlertTemplateProps) => {
+  const alertStyle = {
+    backgroundColor: "white",
+    color:
+      options.type == "error"
+        ? "red"
+        : options.type == "success"
+        ? "#4BB543"
+        : "black",
+    padding: "10px",
+    paddingY: "20px",
+    borderRadius: "10px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    boxShadow: "0px 2px 2px 2px rgba(0, 0, 0, 0.03)",
+    width: "auto",
+    height: "50px",
+    border:
+      options.type == "error"
+        ? "1px solid red"
+        : options.type == "success"
+        ? "1px solid #4BB543"
+        : "1px solid black",
+  };
+
+  const buttonStyle = {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: "20px",
+    border: "none",
+    backgroundColor: "transparent",
+    cursor: "pointer",
+    color: alertStyle.color,
+  };
+
+  return (
+    <div
+      style={{
+        ...alertStyle,
+        ...style,
+        boxSizing: "border-box",
+      }}
+    >
+      <Box
+        sx={{
+          marginRight: "0.5em",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {options.type === "info" && <QuestionMarkIcon />}
+        {options.type === "success" && <CheckCircleIcon />}
+        {options.type === "error" && <ErrorIcon />}
+      </Box>
+      <span style={{ flex: 2 }}>{message}</span>
+      <button onClick={close} style={buttonStyle}>
+        <CloseIcon />
+      </button>
+    </div>
+  );
+};
+
+export default AlertTemplate;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,15 +5,26 @@ import App from "./App";
 import { UserProvider } from "./context/UserContext";
 import "./index.css";
 import { ApartmentProvider } from "./context/ApartmentContext";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "./components/AlertTemplate";
+
+const alertOptions = {
+  position: positions.BOTTOM_RIGHT,
+  timeout: 3000,
+  offset: "15px",
+  transition: transitions.SCALE,
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    // <React.StrictMode>
-    <UserProvider>
-        <ApartmentProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </ApartmentProvider>
-    </UserProvider>
-    // </React.StrictMode>
+  // <React.StrictMode>
+  <UserProvider>
+    <ApartmentProvider>
+      <AlertProvider template={AlertTemplate} {...alertOptions}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AlertProvider>
+    </ApartmentProvider>
+  </UserProvider>
+  // </React.StrictMode>
 );
