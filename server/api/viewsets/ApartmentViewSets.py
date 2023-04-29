0d96b12ae89ea20423904b27e07431bb02b67680
Ziv Attias <ziv.attias7@gmail.com>
2023-04-29 13:22:36 +0300

    patch: (dis)liking apartments bug

--- server/api/viewsets/ApartmentViewSets.py
+++ server/api/viewsets/ApartmentViewSets.py
@@ -103,7 +103,7 @@ def like_apartment(request, apt_id):
     if user.is_authenticated:
         if request.method == "PUT":
             _, created = LikedApartments.objects.get_or_create(
-                user=user, user_profile=user.user_profile, apartment=apartment
+                user=user, apartment=apartment
             )
             if not created:
                 return Response(
@@ -121,9 +121,7 @@ def like_apartment(request, apt_id):
 
         if request.method == "DELETE":
             try:
-                like = LikedApartments.objects.get(
-                    user=user, user_profile=user.user_profile, apartment=apartment
-                )
+                like = LikedApartments.objects.get(user=user, apartment=apartment)
             except LikedApartments.DoesNotExist:
                 return Response(
                     {"message": "Can't unlike a non-liked apartment"},
