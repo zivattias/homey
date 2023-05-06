2394a3c1e40fc0d94fc18e6552a1380ffb6c6d10
Ziv Attias <ziv.attias7@gmail.com>
2023-05-06 14:06:19 +0300

    modify: LinerProgress width responsiveness

--- client/src/components/UploadPage/ProgressBar.tsx
+++ client/src/components/UploadPage/ProgressBar.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import LinearProgress, {
-    LinearProgressProps,
+  LinearProgressProps,
 } from "@mui/material/LinearProgress";
 import { Box, Typography } from "@mui/material";
 
 export default function ProgressBar(
-    props: LinearProgressProps & { value: number }
+  props: LinearProgressProps & { value: number }
 ) {
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginTop: "2em",
-                marginBottom: "1em",
-                flexDirection: "column",
-            }}
-        >
-            <Box sx={{ marginBottom: 1 }}>
-                <Typography variant="body2" color="inherit">
-                    {`${Math.round(props.value)}%`}
-                </Typography>
-            </Box>
-            <Box sx={{ width: "20em" }}>
-                <LinearProgress
-                    variant="determinate"
-                    {...props}
-                    sx={{ borderRadius: "20px" }}
-                />
-            </Box>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "2em",
+        marginBottom: "1em",
+        flexDirection: "column",
+      }}
+    >
+      <Box sx={{ marginBottom: 1 }}>
+        <Typography variant="body2" color="inherit">
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+      <Box sx={{ width: { xs: "50vw", sm: "40vw", md: "25vw" } }}>
+        <LinearProgress
+          variant="determinate"
+          {...props}
+          sx={{ borderRadius: "20px" }}
+        />
+      </Box>
+    </Box>
+  );
 }
