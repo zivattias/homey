bbe2c65543d3ffc20fb271ce7663a8ad32d4f68c
Ziv Attias <ziv.attias7@gmail.com>
2023-05-06 14:06:51 +0300

    patch: code refactor

--- client/src/components/UploadPage/FirstStage.tsx
+++ client/src/components/UploadPage/FirstStage.tsx
@@ -1,135 +1,131 @@
 import React from "react";
 import {
-    Box,
-    Button,
-    Container,
-    Divider,
-    Stack,
-    TextField,
-    Tooltip,
-    Typography,
+  Box,
+  Button,
+  Container,
+  Divider,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
 } from "@mui/material";
 import { Apartment, useApartment } from "../../context/ApartmentContext";
 import { Theme } from "@mui/material/styles/createTheme";
 import ProgressBar from "./ProgressBar";
 
 function FirstStage({
-    theme,
-    handleChange,
-    handleStages,
+  theme,
+  handleChange,
+  handleStages,
 }: {
-    theme: Theme;
-    handleChange(
-        attr: keyof Apartment
-    ): (event: React.ChangeEvent<HTMLInputElement>) => void;
-    handleStages: (event: React.FormEvent) => void;
+  theme: Theme;
+  handleChange(
+    attr: keyof Apartment
+  ): (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleStages: (event: React.FormEvent) => void;
 }) {
-    const apartment = useApartment();
+  const apartment = useApartment();
 
-    return (
-        <Container
-            sx={{
-                px: 4,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                marginBottom: "3em",
-            }}
-        >
-            <Box
+  return (
+    <Container
+      sx={{
+        px: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: "3em",
+      }}
+    >
+      <Box
+        sx={{
+          borderRadius: "10px",
+          border:
+            theme.palette.mode === "dark"
+              ? "1px solid rgba(256, 256, 256, .4)"
+              : "1px solid rgba(0, 0, 0, .4)",
+          p: 3,
+        }}
+      >
+        <Stack gap={2}>
+          <Typography component="h1" variant="h5">
+            Add your apartment in just a few clicks!
+          </Typography>
+          <Typography>
+            Fill out this form to add your apartment to your account. You will
+            be able to {""}
+            <Tooltip title="* Display your apartment on Homey">
+              <Typography
+                component="span"
                 sx={{
-                    borderRadius: "10px",
-                    border:
-                        theme.palette.mode === "dark"
-                            ? "1px solid rgba(256, 256, 256, .4)"
-                            : "1px solid rgba(0, 0, 0, .4)",
-                    p: 3,
+                  display: "inline",
+                  fontWeight: "bold",
                 }}
-            >
-                <Stack gap={2}>
-                    <Typography component="h1" variant="h5">
-                        Add your apartment in just a few clicks!
-                    </Typography>
-                    <Typography>
-                        Fill out this form to add your apartment to your
-                        account. You will be able to {""}
-                        <Tooltip title="* Display your apartment on Homey">
-                            <Typography
-                                component="span"
-                                sx={{
-                                    display: "inline",
-                                    fontWeight: "bold",
-                                }}
-                            >
-                                activate*
-                            </Typography>
-                        </Tooltip>
-                        {""} it later in the process.
-                    </Typography>
-                    <Box
-                        component="form"
-                        sx={{ display: "flex", flexDirection: "column" }}
-                        onSubmit={(event) => handleStages(event)}
-                    >
-                        <TextField
-                            sx={{ width: "100%", mb: "1em" }}
-                            value={apartment.street}
-                            onChange={handleChange("street")}
-                            id="street"
-                            label="Street Name"
-                            variant="outlined"
-                        />
-                        <TextField
-                            type="number"
-                            sx={{ width: "100%", mb: "1em" }}
-                            value={apartment.streetNum ?? ""}
-                            onChange={handleChange("streetNum")}
-                            id="street_num"
-                            label="Street Number"
-                            variant="outlined"
-                        />
-                        <TextField
-                            sx={{ width: "100%", mb: "1em" }}
-                            type="number"
-                            value={apartment.aptNum ?? ""}
-                            onChange={handleChange("aptNum")}
-                            id="apt_num"
-                            label="Apartment Number"
-                            variant="outlined"
-                        />
-                        <Divider></Divider>
-                        <TextField
-                            sx={{ width: "100%", my: "1em" }}
-                            type="number"
-                            value={apartment.zipCode}
-                            onChange={handleChange("zipCode")}
-                            id="zip_code"
-                            label="Zip Code"
-                            variant="outlined"
-                        />
-                        <TextField
-                            sx={{ width: "100%", mb: "1em" }}
-                            type="number"
-                            value={apartment.squareMeter ?? ""}
-                            onChange={handleChange("squareMeter")}
-                            id="square_meter"
-                            label="Square Meter"
-                            variant="outlined"
-                        />
-                        <Divider></Divider>
-                        <Button
-                            sx={{ my: "1em" }}
-                            variant="contained"
-                            type="submit"
-                        >
-                            Next
-                        </Button>
-                    </Box>
-                </Stack>
-            </Box>
-        </Container>
-    );
+              >
+                activate*
+              </Typography>
+            </Tooltip>
+            {""} it later in the process.
+          </Typography>
+          <Box
+            component="form"
+            sx={{ display: "flex", flexDirection: "column" }}
+            onSubmit={(event) => handleStages(event)}
+          >
+            <TextField
+              sx={{ width: "100%", mb: "1em" }}
+              value={apartment.street}
+              onChange={handleChange("street")}
+              id="street"
+              label="Street Name"
+              variant="outlined"
+            />
+            <TextField
+              type="number"
+              sx={{ width: "100%", mb: "1em" }}
+              value={apartment.streetNum ?? ""}
+              onChange={handleChange("streetNum")}
+              id="street_num"
+              label="Street Number"
+              variant="outlined"
+            />
+            <TextField
+              sx={{ width: "100%", mb: "1em" }}
+              type="number"
+              value={apartment.aptNum ?? ""}
+              onChange={handleChange("aptNum")}
+              id="apt_num"
+              label="Apartment Number"
+              variant="outlined"
+            />
+            <Divider></Divider>
+            <TextField
+              sx={{ width: "100%", my: "1em" }}
+              type="number"
+              value={apartment.zipCode}
+              onChange={handleChange("zipCode")}
+              id="zip_code"
+              label="Zip Code"
+              variant="outlined"
+            />
+            <TextField
+              sx={{ width: "100%", mb: "1em" }}
+              type="number"
+              value={apartment.squareMeter ?? ""}
+              onChange={handleChange("squareMeter")}
+              id="square_meter"
+              label="Square Meter"
+              variant="outlined"
+            />
+            <Divider></Divider>
+            <Button sx={{ my: "1em" }} variant="contained" type="submit">
+              Next
+            </Button>
+          </Box>
+        </Stack>
+      </Box>
+    </Container>
+  );
 }
 
 export default FirstStage;
--- client/src/pages/UploadPage.tsx
+++ client/src/pages/UploadPage.tsx
@@ -3,76 +3,76 @@ import { useUser } from "../context/UserContext";
 import { Box, Container, useTheme } from "@mui/material";
 import { Navigate } from "react-router-dom";
 import {
-    ADD_APARTMENT_ACTIONS,
-    Apartment,
-    useApartmentDispatch,
+  ADD_APARTMENT_ACTIONS,
+  Apartment,
+  useApartmentDispatch,
 } from "../context/ApartmentContext";
 import FirstStage from "../components/UploadPage/FirstStage";
 import SecondStage from "../components/UploadPage/SecondStage";
 import ProgressBar from "../components/UploadPage/ProgressBar";
 
 function UploadPage() {
-    const theme = useTheme();
-    const user = useUser();
-    const dispatch = useApartmentDispatch();
-    const totalStages = 2;
-    const [isSecondStage, setIsSecondStage] = React.useState<boolean>(false);
-    const [atStage, setAtStage] = React.useState<number>(totalStages);
-    const [progressBarValue, setProgressBarValue] = React.useState<number>(0);
+  const theme = useTheme();
+  const user = useUser();
+  const dispatch = useApartmentDispatch();
+  const totalStages = 2;
+  const [isSecondStage, setIsSecondStage] = React.useState<boolean>(false);
+  const [atStage, setAtStage] = React.useState<number>(totalStages);
+  const [progressBarValue, setProgressBarValue] = React.useState<number>(0);
 
-    React.useEffect(() => setProgressBarValue(100 / atStage), [atStage]);
+  React.useEffect(() => setProgressBarValue(100 / atStage), [atStage]);
 
-    const handleStages = (event: React.FormEvent) => {
-        event.preventDefault();
-        isSecondStage
-            ? (setIsSecondStage(false), setAtStage(2))
-            : (setIsSecondStage(true), setAtStage(1));
+  const handleStages = (event: React.FormEvent) => {
+    event.preventDefault();
+    isSecondStage
+      ? (setIsSecondStage(false), setAtStage(2))
+      : (setIsSecondStage(true), setAtStage(1));
+  };
+
+  function handleChange(attr: keyof Apartment) {
+    return (event: React.ChangeEvent<HTMLInputElement>) => {
+      let value: any = event.target.value;
+      if (["streetNum", "aptNum", "squareMeter"].includes(attr)) {
+        value = parseInt(value);
+      }
+      dispatch({
+        type: ADD_APARTMENT_ACTIONS.CHANGE_ATTR,
+        payload: { [attr]: value },
+      });
     };
+  }
 
-    function handleChange(attr: keyof Apartment) {
-        return (event: React.ChangeEvent<HTMLInputElement>) => {
-            let value: any = event.target.value;
-            if (["streetNum", "aptNum", "squareMeter"].includes(attr)) {
-                value = parseInt(value);
-            }
-            dispatch({
-                type: ADD_APARTMENT_ACTIONS.CHANGE_ATTR,
-                payload: { [attr]: value },
-            });
-        };
-    }
-
-    return !user.accessToken ? (
-        <Navigate to="/" />
-    ) : (
-        <Box
-            sx={{
-                height: {
-                    // 100vh - navBar.height
-                    xs: "calc(100vh - 56px)",
-                    sm: "calc(100vh - 64px)",
-                    md: "calc(100vh - 88px)",
-                    lg: "calc(100vh - 88px)",
-                },
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <ProgressBar value={progressBarValue} />
-            {isSecondStage ? (
-                <SecondStage theme={theme} handleStages={handleStages} />
-            ) : (
-                <FirstStage
-                    theme={theme}
-                    handleChange={handleChange}
-                    handleStages={handleStages}
-                />
-            )}
-        </Box>
-    );
+  return !user.accessToken ? (
+    <Navigate to="/" />
+  ) : (
+    <Box
+      sx={{
+        // height: {
+        //   // 100vh - navBar.height
+        //   xs: "calc(100vh - 56px)",
+        //   sm: "calc(100vh - 64px)",
+        //   md: "calc(100vh - 88px)",
+        //   lg: "calc(100vh - 88px)",
+        // },
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ProgressBar value={progressBarValue} />
+      {isSecondStage ? (
+        <SecondStage theme={theme} handleStages={handleStages} />
+      ) : (
+        <FirstStage
+          theme={theme}
+          handleChange={handleChange}
+          handleStages={handleStages}
+        />
+      )}
+    </Box>
+  );
 }
 
 export default UploadPage;
