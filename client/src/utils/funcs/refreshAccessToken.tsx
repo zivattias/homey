92c48c9d4404b822f02d054c9c7b10d58a713763
Ziv Attias <ziv.attias7@gmail.com>
2023-05-06 14:01:46 +0300

    patch: refresh tokens error handling -> UI alert

--- client/src/utils/funcs/refreshAccessToken.tsx
+++ client/src/utils/funcs/refreshAccessToken.tsx
@@ -2,26 +2,22 @@ import axios, { AxiosError } from "axios";
 import { API_ENDPOINTS, FULL_API_ENDPOINT } from "../consts";
 
 async function refreshAccessToken(refreshToken: string) {
-    try {
-        const response = await axios.post(
-            FULL_API_ENDPOINT +
-                API_ENDPOINTS.AUTH.BASE +
-                API_ENDPOINTS.AUTH.REFRESH,
-            { refresh: refreshToken }
-        );
-        return response.data.access;
-    } catch (e) {
-        if (e instanceof AxiosError) {
-            if (e.response) {
-                console.log(
-                    "AxiosError: " + e.response.status,
-                    e.response.data
-                );
-            }
-        } else {
-            throw new Error(String(e));
-        }
+  try {
+    const response = await axios.post(
+      FULL_API_ENDPOINT + API_ENDPOINTS.AUTH.BASE + API_ENDPOINTS.AUTH.REFRESH,
+      { refresh: refreshToken }
+    );
+    return response.data.access;
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      if (e.response) {
+        console.log("AxiosError: " + e.response.status, e.response.data);
+      }
+      console.log("Removing invalid refreshToken from localStorage");
+      localStorage.removeItem("refreshToken");
+      throw new Error();
     }
+  }
 }
 
 export default refreshAccessToken;
--- client/src/utils/funcs/sendRequest.tsx
+++ client/src/utils/funcs/sendRequest.tsx
@@ -1,21 +1,17 @@
 import axios, { AxiosResponse } from "axios";
 
-async function sendRequest(
-    method: "post" | "get" | "patch" | "put" | "delete",
-    url: string,
-    accessToken: string,
-    data: { [key: string]: string }
+export default async function sendRequest(
+  method: "post" | "get" | "patch" | "put" | "delete",
+  url: string,
+  accessToken: string,
+  data: { [key: string]: string }
 ): Promise<AxiosResponse> {
-    const headers = accessToken
-        ? { Authorization: `Bearer ${accessToken}` }
-        : {};
-    const response = await axios({
-        method: method,
-        url: url,
-        data: data,
-        headers: headers,
-    });
-    return response;
+  const headers = accessToken ? { Authorization: `Bearer ${accessToken}` } : {};
+  const response = await axios({
+    method: method,
+    url: url,
+    data: data,
+    headers: headers,
+  });
+  return response;
 }
-
-export default sendRequest;
