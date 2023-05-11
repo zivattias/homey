5fcade41ace07857804a2231e0a7688c826d8ecd
Ziv Attias <ziv.attias7@gmail.com>
2023-05-11 16:22:15 +0300

    add: change user password REST ability

--- server/api/serializers/UserSerializer.py
+++ server/api/serializers/UserSerializer.py
@@ -1,5 +1,7 @@
-from rest_framework import serializers
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
 
 from .ApartmentSerializer import LikedApartmentsSerializer
 
@@ -7,6 +9,10 @@ from .ApartmentSerializer import LikedApartmentsSerializer
 class UserSerializer(serializers.ModelSerializer):
     liked_apartments = LikedApartmentsSerializer
     profile_pic = serializers.SerializerMethodField()
+    password = serializers.CharField(
+        write_only=True, required=False, validators=[validate_password]
+    )
+    confirm_password = serializers.CharField(write_only=True, required=False)
 
     def get_profile_pic(self, obj):
         return obj.user_profile.profile_pic
@@ -25,4 +31,35 @@ class UserSerializer(serializers.ModelSerializer):
             "is_staff",
             "profile_pic",
             "liked_apartments",
+            "password",
+            "confirm_password",
         )
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "confirm_password": {"write_only": True},
+        }
+
+    def validate(self, attrs):
+        if set(attrs.keys()) == set(["password", "confirm_password"]):
+            if attrs["password"] != attrs["confirm_password"]:
+                raise serializers.ValidationError(
+                    {"confirm_password": "Password fields don't match."}
+                )
+
+            try:
+                validate_password(attrs["password"])
+            except ValidationError as e:
+                raise serializers.ValidationError({"password": e.messages})
+
+        return attrs
+
+    def update(self, instance: User, validated_data):
+        if set(validated_data.keys()) == set(["password", "confirm_password"]):
+            instance.set_password(validated_data["password"])
+            instance.save()
+            return instance
+
+        _, _ = validated_data.pop("password", None), validated_data.pop(
+            "confirm_password", None
+        )
+        return super().update(instance, validated_data)
